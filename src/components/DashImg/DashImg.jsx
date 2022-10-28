058963927dc078571d904cc811a9ba230c637963
stella-mary <mariastellamary@outlook.com>
2022-10-28 05:53:24 +0530

    Dashboard, dashImg file added

--- src/components/DashImg/DashImg.jsx
+++ src/components/DashImg/DashImg.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import './DashImg.scss'
 
 export default function DashImg() {
     return (
         <div className='DashImg'>
             <img
-                src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=600"
+                src="https://wallpapercave.com/wp/nsu3cSp.jpg"
                 alt=""
                 className="avatar"
             />
+            <div className='single__data__data'>
+                <div className="single__data">
+                    <div className="data__content">
+                        <h4>Google <span>Google Inc.</span></h4>
+                        <p className="data__text">The search engine giant</p>
+                        <p className="data__text__para">Czech Republic</p>
+                        <button>+3.53%</button>
+                    </div>
+                </div>
+                <div className="single__data__content">
+                    <div className="data__content">
+                        <h4>Google <span>Google Inc.</span></h4>
+                        <p className="data__text">The search engine giant</p>
+                        <p className="data__text__para">Czech Republic</p>
+                        <button>+3.53%</button>
+                    </div>
+                </div>
+            </div>
+            <div className='single__data__performance'>
+
+            </div>
         </div>
     )
 }
